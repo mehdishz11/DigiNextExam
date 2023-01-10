ddbaecd937531376c896356be83168361e1a8855
Mehdi Shojaeian <mehdi_shz11@yahoo.com>
2023-01-10 22:06:44 +0330

    Second Quiz

--- src/main/kotlin/Exam2.kt
+++ src/main/kotlin/Exam2.kt
@@ -1,12 +1,12 @@
 fun main() {
-    println(findMinRemovable(""))
+    println(findMinRemovable("ABABA"))
 }
 
 fun findMinRemovable(inputString: String): Int {
     if (inputString.isEmpty()) {
         return 0
     }
-    val firstChar = inputString.toCharArray()[0]
+    val firstChar = inputString[0]
     val replace = if (firstChar == 'A') {
         "AB"
     } else {
