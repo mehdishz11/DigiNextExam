7e17f0ffd9e4b4311036f3f28d9b08b9873e7402
mehdishz11 <psb.ltd@aol.com>
2023-02-01 15:15:50 +0000

    Feature3 (#7)
    * Feature 3 commit 1,2
    
    * Feature 1 commit 4
    
    * Feature 1 commit 5
    
    * Feature 1 commit 5
    
    * Feature 1 commit 1
    
    * Feature 3

--- src/main/kotlin/testForKotlinGeneric/OurGenericClass.kt
+++ src/main/kotlin/testForKotlinGeneric/OurGenericClass.kt
@@ -44,10 +44,7 @@ class MyClass{
         }
 
         val a1:MyGeneric<out Base2> = MyGeneric<Base2>()
-        //some changes 1 in feature 3
-        //some changes 2 in feature 3
-        //some changes 2 in feature 4
-        //some changes 2 in feature 5
+        //New changes on feature 3
 
 
 
